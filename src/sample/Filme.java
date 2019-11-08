f982b29fa84524f491ebf744c83149b1ae4a2160
Ronims <davialvesviana@hotmail.com>
2019-11-08 15:39:01 -0300

    teste

---
+++ src/sample/Filme.java
@@ -0,0 +1,87 @@
+package sample;
+
+public class Filme {
+
+    private boolean acao;
+    private boolean aventura;
+    private boolean drama;
+    private boolean historico;
+    private boolean romance;
+    private boolean cientifico;
+    private boolean documentario;
+    private boolean comedia;
+    private boolean pretobranco;
+
+    public boolean isAcao() {
+        return acao;
+    }
+
+    public void setAcao(boolean acao) {
+        this.acao = acao;
+    }
+
+    public boolean isAventura() {
+        return aventura;
+    }
+
+    public void setAventura(boolean aventura) {
+        this.aventura = aventura;
+    }
+
+    public boolean isDrama() {
+        return drama;
+    }
+
+    public void setDrama(boolean drama) {
+        this.drama = drama;
+    }
+
+    public boolean isHistorico() {
+        return historico;
+    }
+
+    public void setHistorico(boolean historico) {
+        this.historico = historico;
+    }
+
+    public boolean isRomance() {
+        return romance;
+    }
+
+    public void setRomance(boolean romance) {
+        this.romance = romance;
+    }
+
+    public boolean isCientifico() {
+        return cientifico;
+    }
+
+    public void setCientifico(boolean cientifico) {
+        this.cientifico = cientifico;
+    }
+
+    public boolean isDocumentario() {
+        return documentario;
+    }
+
+    public void setDocumentario(boolean documentario) {
+        this.documentario = documentario;
+    }
+
+    public boolean isComedia() {
+        return comedia;
+    }
+
+    public void setComedia(boolean comedia) {
+        this.comedia = comedia;
+    }
+
+    public boolean isPretobranco() {
+        return pretobranco;
+    }
+
+    public void setPretobranco(boolean pretobranco) {
+        this.pretobranco = pretobranco;
+    }
+
+}
---
+++ src/sample/Musica.java
@@ -0,0 +1,87 @@
+package sample;
+
+public class Musica {
+
+    private boolean rock;
+    private boolean pop;
+    private boolean rap;
+    private boolean kpop;
+    private boolean jpop;
+    private boolean indie;
+    private boolean heavymetal;
+    private boolean emo;
+    private boolean hiphop;
+
+    public boolean getRock() {
+        return rock;
+    }
+
+    public void setRock(boolean rock) {
+        this.rock = rock;
+    }
+
+    public boolean getPop() {
+        return pop;
+    }
+
+    public void setPop(boolean pop) {
+        this.pop = pop;
+    }
+
+    public boolean getRap() {
+        return rap;
+    }
+
+    public void setRap(boolean rap) {
+        this.rap = rap;
+    }
+
+    public boolean getKpop() {
+        return kpop;
+    }
+
+    public void setKpop(boolean kpop) {
+        this.kpop = kpop;
+    }
+
+    public boolean getJpop() {
+        return jpop;
+    }
+
+    public void setJpop(boolean jpop) {
+        this.jpop = jpop;
+    }
+
+    public boolean getIndie() {
+        return indie;
+    }
+
+    public void setIndie(boolean indie) {
+        this.indie = indie;
+    }
+
+    public boolean getHeavymetal() {
+        return heavymetal;
+    }
+
+    public void setHeavymetal(boolean heavymetal) {
+        this.heavymetal = heavymetal;
+    }
+
+    public boolean getEmo() {
+        return emo;
+    }
+
+    public void setEmo(boolean emo) {
+        this.emo = emo;
+    }
+
+    public boolean getHiphop() {
+        return hiphop;
+    }
+
+    public void setHiphop(boolean hiphop) {
+        this.hiphop = hiphop;
+    }
+
+}
